d4274bfb17dd29a2e2f98d42a48264d058e26d7f
tsusuga <tsusuga@j-com.co.jp>
2023-02-14 11:32:46 +0900

    webDriverWaitの導入

--- jobcan_auto.py
+++ jobcan_auto.py
@@ -1,8 +1,10 @@
 import chromedriver_binary
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome import service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
 import time
 from dotenv import load_dotenv
 import os
@@ -15,17 +17,23 @@ options.add_experimental_option('excludeSwitches', ['enable-logging'])
 print('connectiong to remote browser...')
 driver = webdriver.Chrome(options=options)
 
-driver.get('https://id.jobcan.jp/users/sign_in?app_key=atd')
+url = 'https://id.jobcan.jp/users/sign_in?app_key=atd'
+driver.get(url)
 print(driver.current_url)
 
-time.sleep(1)
+wait = WebDriverWait(driver, 10)
+
 
 # ログイン情報入力欄の要素取得
-id_input = driver.find_element_by_xpath("/html/body/div[@class='col-sm-6 sign-in-left-container']/div[@class='sign-in-panels-login']/div[@class='panel panel-default']/div[@class='panel-body']/form[@id='new_user']/div[@id='login_input']/label[@class='form__label'][1]/input[@id='user_email']")
+# id_xpath = "/html/body/div[@class='col-sm-6 sign-in-left-container']/div[@class='sign-in-panels-login']/div[@class='panel panel-default']/div[@class='panel-body']/form[@id='new_user']/div[@id='login_input']/label[@class='form__label'][1]/input[@id='user_email']"
 
-pw_input = driver.find_element_by_xpath("/html/body/div[@class='col-sm-6 sign-in-left-container']/div[@class='sign-in-panels-login']/div[@class='panel panel-default']/div[@class='panel-body']/form[@id='new_user']/div[@id='login_input']/label[@class='form__label'][2]/input[@id='user_password']")
+id_input = wait.until(EC.visibility_of_element_located(By.ID, 'user_email'))
 
-login_button = driver.find_element_by_xpath("/html/body/div[@class='col-sm-6 sign-in-left-container']/div[@class='sign-in-panels-login']/div[@class='panel panel-default']/div[@class='panel-body']/form[@id='new_user']/input[@id='login_button']")
+# id_input = driver.find_element_by_xpath("/html/body/div[@class='col-sm-6 sign-in-left-container']/div[@class='sign-in-panels-login']/div[@class='panel panel-default']/div[@class='panel-body']/form[@id='new_user']/div[@id='login_input']/label[@class='form__label'][1]/input[@id='user_email']")
+
+# pw_input = driver.find_element_by_xpath("/html/body/div[@class='col-sm-6 sign-in-left-container']/div[@class='sign-in-panels-login']/div[@class='panel panel-default']/div[@class='panel-body']/form[@id='new_user']/div[@id='login_input']/label[@class='form__label'][2]/input[@id='user_password']")
+
+# login_button = driver.find_element_by_xpath("/html/body/div[@class='col-sm-6 sign-in-left-container']/div[@class='sign-in-panels-login']/div[@class='panel panel-default']/div[@class='panel-body']/form[@id='new_user']/input[@id='login_button']")
 
 # load_dotenv()
 # account_id = os.getenv('ID')
