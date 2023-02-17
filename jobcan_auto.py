f0980488fc652416de2a74034de118285ea1f967
tsusuga <tsusuga@j-com.co.jp>
2023-02-17 15:24:50 +0900

    修正

--- jobcan_auto.py
+++ jobcan_auto.py
@@ -9,7 +9,6 @@ import time
 from dotenv import load_dotenv
 import os
 
-
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 # options.add_argument('--headless')
@@ -21,25 +20,24 @@ url = 'https://id.jobcan.jp/users/sign_in?app_key=atd'
 driver.get(url)
 print(driver.current_url)
 
+#ページ上のすべての要素が読み込まれるまで待機
 wait = WebDriverWait(driver, 10)
+wait.until(EC.presence_of_all_elements_located)
+#入力フォームを取得
+id_input = driver.find_element_by_id('user_email')
+pw_input = driver.find_element_by_id('user_password')
+login_button = driver.find_element_by_id('login_button')
+#.envファイルからログイン情報の取得
+load_dotenv()
+account_id = os.getenv('ID')
+account_pw = os.getenv('PW')
+#フォームにログイン情報を入力してログインボタンをPUSH
+id_input.send_keys(account_id)
+pw_input.send_keys(account_pw)
+login_button.send_keys(Keys.ENTER)
+#打刻ボタンを取得してPUSH
+wait.until(EC.presence_of_all_elements_located)
+audit_button = driver.find_element_by_id('adit-button-push')
+audit_button.send_keys(Keys.ENTER)
 
-
-# ログイン情報入力欄の要素取得
-# id_xpath = "/html/body/div[@class='col-sm-6 sign-in-left-container']/div[@class='sign-in-panels-login']/div[@class='panel panel-default']/div[@class='panel-body']/form[@id='new_user']/div[@id='login_input']/label[@class='form__label'][1]/input[@id='user_email']"
-
-id_input = wait.until(EC.visibility_of_element_located(By.ID, 'user_email'))
-
-# id_input = driver.find_element_by_xpath("/html/body/div[@class='col-sm-6 sign-in-left-container']/div[@class='sign-in-panels-login']/div[@class='panel panel-default']/div[@class='panel-body']/form[@id='new_user']/div[@id='login_input']/label[@class='form__label'][1]/input[@id='user_email']")
-
-# pw_input = driver.find_element_by_xpath("/html/body/div[@class='col-sm-6 sign-in-left-container']/div[@class='sign-in-panels-login']/div[@class='panel panel-default']/div[@class='panel-body']/form[@id='new_user']/div[@id='login_input']/label[@class='form__label'][2]/input[@id='user_password']")
-
-# login_button = driver.find_element_by_xpath("/html/body/div[@class='col-sm-6 sign-in-left-container']/div[@class='sign-in-panels-login']/div[@class='panel panel-default']/div[@class='panel-body']/form[@id='new_user']/input[@id='login_button']")
-
-# load_dotenv()
-# account_id = os.getenv('ID')
-# account_pw = os.getenv('PW')
-
-id_input.send_keys("avs")
-# pw_input.send_keys("ddd")
-
-# login_button.send_keys(Keys.ENTER)
+driver.quit()
